6ba0163bd9fdd12dbcdcbd639284fea4e6ba9013
Cruelas <irula5198@gmail.com>
2024-06-13 16:16:50 +0700

    commit last

--- BinarySearchTree.cpp
+++ BinarySearchTree.cpp
@@ -110,7 +110,7 @@ int main ()
         cout << "2. Perform inorder traversal" << endl;
         cout << "3. Perform preorder traversal" << endl;
         cout << "4. Perform postorder traversal" << endl;
-        cout << "5.Exit" << endl;
+        cout << "5. Exit" << endl;
         cout << "\nEnter your choice (1-5) :" << endl;
 
         char ch;
@@ -141,6 +141,14 @@ int main ()
             {
                 x.postorder(x.ROOT);
                 break;
+            }
+            case '5' :
+            {
+               return 0;
+            }
+            default:
+            {
+                cout << "Invalid Option" << endl;
             }
         }
     }
